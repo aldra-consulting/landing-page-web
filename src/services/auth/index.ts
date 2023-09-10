d614041eedcc2d2731c61683a5716069876eccc3
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-09-10 22:54:15 +0200

    feat: add auth service

---
+++ src/services/auth/index.ts
@@ -0,0 +1,21 @@
+import { User, UserManager } from 'oidc-client-ts';
+
+export default class AuthService {
+  #manager: UserManager;
+
+  constructor(manager: UserManager) {
+    this.#manager = manager;
+  }
+
+  signIn = async (): Promise<void> => this.#manager.signinRedirect();
+
+  signOut = async (): Promise<void> => this.#manager.signoutRedirect();
+
+  getUser = async (): Promise<User | null> => this.#manager.getUser();
+
+  completeSignIn = async (): Promise<User> =>
+    this.#manager.signinRedirectCallback();
+
+  completeSilentSignIn = async (): Promise<void> =>
+    this.#manager.signinSilentCallback();
+}
