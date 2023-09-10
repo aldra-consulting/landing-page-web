aac4fd6f696e0307ded3964f9001d2110c1e6030
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-09-10 16:11:24 +0200

    feat: add avatar component

---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,19 @@
+import { HTMLAttributes, component$, useStylesScoped$ } from '@builder.io/qwik';
+
+import styles from './styles.css?inline';
+
+interface Props extends HTMLAttributes<HTMLElement> {}
+
+export default component$<Props>(({ title, ...props }) => {
+  useStylesScoped$(styles);
+
+  return (
+    <abbr title={title} {...props}>
+      {title
+        ?.match(/\b(\w)/g)
+        ?.join('')
+        .toUpperCase()
+        .substring(0, 2)}
+    </abbr>
+  );
+});
