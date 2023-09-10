84e43e4c03302a6700141b222bfbdcf0c62492b6
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-09-10 17:24:07 +0200

    feat: add sign in button component

---
+++ src/components/sign-in-button/index.tsx
@@ -0,0 +1,20 @@
+import {
+  HTMLAttributes,
+  Slot,
+  component$,
+  useStylesScoped$
+} from '@builder.io/qwik';
+
+import styles from './styles.css?inline';
+
+interface Props extends HTMLAttributes<HTMLButtonElement> {}
+
+export default component$<Props>(({ ...props }) => {
+  useStylesScoped$(styles);
+
+  return (
+    <button type='button' {...props}>
+      <Slot />
+    </button>
+  );
+});
