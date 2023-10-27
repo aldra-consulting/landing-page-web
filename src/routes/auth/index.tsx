893408915d3c49caf24b50ac7dd801a851c6efda
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-10-27 16:38:22 +0200

    fix: remove navigation

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -3,11 +3,9 @@ import { $, component$, useOnWindow } from '@builder.io/qwik';
 import { auth } from '@app/utils/auth';
 
 export default component$(() => {
-  const navigateToRoot = $(() => window.location.replace('/'));
-
   useOnWindow(
     'load',
-    $(() => auth().completeSignIn().finally(navigateToRoot))
+    $(() => auth().completeSignIn())
   );
 
   return <span hidden />;
