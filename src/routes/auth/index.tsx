1b65b6bfa1d478877c0f75e6b16583cd994b03c7
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-10-28 16:09:05 +0200

    fix: bring back navigation

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -3,9 +3,16 @@ import { $, component$, useOnWindow } from '@builder.io/qwik';
 import { auth } from '@app/utils/auth';
 
 export default component$(() => {
+  const navigateToRoot = $(() => window.location.replace('/'));
+
   useOnWindow(
     'load',
-    $(() => auth().completeSignIn())
+    $(() =>
+      auth()
+        .completeSignIn()
+        .catch(() => {})
+        .finally(navigateToRoot)
+    )
   );
 
   return <span hidden />;
